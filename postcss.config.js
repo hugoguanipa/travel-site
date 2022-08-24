5ceabf99413d5e81bf4e197fe07f6d82f2e80ecb
Hugo <hugoguanipa.dev@gmail.com>
2022-08-24 11:07:37 -0500

    Mixins, Clamp, postcss-calc added

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,13 @@
+const postcssMixins = require("postcss-mixins");
+
 module.exports = {
     plugins: [
         'postcss-simple-vars',
         'postcss-nested',
         'autoprefixer',
         'postcss-import',
-        'cssnano'
+        'cssnano',
+        'postcss-mixins',
+        'postcss-calc'
     ],
   };
