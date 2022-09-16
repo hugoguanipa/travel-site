54e298ab84947d9582017577d948036400857ee5
hugoguanipa <hugoguanipa.dev@gmail.com>
2022-09-15 23:07:08 -0500

    Smooth header and Highlighted navigation menu

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,13 +1,16 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 class StickyHeader {
     constructor() {
         this.siteHeader = document.querySelector(".site-header")
         this.pageSections = document.querySelectorAll(".page-section")
+        this.linksActivation()
         this.events()
+        
     }
 
     events() {
@@ -17,7 +20,32 @@ class StickyHeader {
             toggleClass: {className: 'site-header--scrolled', targets: '.site-header '}
           });
     }
-    
+
+    linksActivation() {
+        let links = gsap.utils.toArray("nav a");
+        links.forEach(a => {
+          let element = document.querySelector(a.getAttribute("href")),
+              linkST = ScrollTrigger.create({
+                    trigger: element,
+                    start: "top top"
+                  });
+          ScrollTrigger.create({
+            trigger: element,
+            start: "top center",
+            end: "bottom center",
+            onToggle: self => self.isActive && setActive(a)
+          });
+          a.addEventListener("click", e => {
+            e.preventDefault();
+            gsap.to(window, {duration: 1, scrollTo: linkST.start, overwrite: "auto"});
+          });
+        });
+        
+        function setActive(link) {
+          links.forEach(el => el.classList.remove("active"));
+          link.classList.add("active");
+        }
+    }
 }
 
 export default StickyHeader
